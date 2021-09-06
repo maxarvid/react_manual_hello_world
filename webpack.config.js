4b4a5390364de51b8ee4597df62513bf7e7be38e
maxarvid <max.arvid.anderson@gmail.com>
2021-09-06 16:39:57 +0200

    adds webpack and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const webpack = require("webpack");
+const uglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extentions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirnamem, "dist/"),
+    publicPath: "/dist",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    watchContentBase: true,
+    historyApiFallback: true,
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: {
+            keep_frames: true,
+          }
+        }
+      })
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
